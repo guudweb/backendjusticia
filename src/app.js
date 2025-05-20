3819efbb2969adf9bfc146a020c564bf5da248b2
edu lenovo <eduardochamacoelguapo@gmail.com>
2025-05-20 18:45:16 +0100

    Initial commit - First version of the backend 3

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/authroutes.js";
-import noticiasRoutes from "./routes/noticiasRoutes.js";
+import noticiasRoutes from "./routes/noticiasroutes.js";
 
 // Cargar variables de entorno
 dotenv.config();
